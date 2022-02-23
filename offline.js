cdb80efb5409efde4e714167193b20afa3855368
Sans <83004239+mario1D240@users.noreply.github.com>
2022-02-23 14:06:17 +0800

    Update offline.js

--- offline.js
+++ offline.js
@@ -81,7 +81,7 @@
 		"media/damage.ogg",
 		"media/aaaaaa.ogg",
 		"media/gu.ogg",
-		"media/mus_zz_megalovania.wav",
+		"media/mus_zz_megalovania.webm",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
